32b50c027ae44b2946f17c0e2c70d75dc9e320c2
Mihanta <35023484+Mihanta@users.noreply.github.com>
2018-01-15 08:44:16 +0300

    Add files via upload

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,pag:0,recherch:0},titles:["Welcome to tutorial intercept\u2019s documentation!"],titleterms:{and:0,document:0,indic:0,intercept:0,tabl:0,tutorial:0,welcom:0}})
